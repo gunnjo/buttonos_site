ba0d0aebcdda66cab4989e54b0f7f46df6757348
Somebody, On Production <root@curlycloud.com>
2011-12-12 10:42:56 +0000

    Help is not start

--- application/classes/controller/help.php
+++ application/classes/controller/help.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Start extends Controller_Global {
+class Controller_Start_Help extends Controller_Global {
 
 
 	public function before()
@@ -14,7 +14,7 @@ class Controller_Start extends Controller_Global {
 	public function action_index()
 	{
 
-		$this->body = View::factory('start');
+		$this->body = View::factory('help');
 		$this->body->set(
 			array(
 				'page_title' => 'Welcome to ButtonOS - How can we help you?'
